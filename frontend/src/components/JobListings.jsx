9d87fc5905ed3a90c04f7b08915d0444975d5f8e
mahnoorf <Mahnoor.Fatima@metropolia.fi>
2024-09-30 10:43:50 +0300

    Part 1: Iteration 3 &4 added

--- frontend/src/components/JobListings.jsx
+++ frontend/src/components/JobListings.jsx
@@ -1,10 +1,18 @@
-import JobListing from "./JobListing";
+import { Link } from "react-router-dom";
 
 const JobListings = ({ jobs }) => {
   return (
     <div className="job-list">
       {jobs.map((job) => (
-        <JobListing key={job.id} job={job} />
+        <div className="job-preview" key={job.id}>
+          <Link to={`/jobs/${job.id}`}>
+            <h2>{job.title}</h2>
+          </Link>
+          <h2>{job.title}</h2>
+          <p>Type: {job.type}</p>
+          <p>Description: {job.description}</p>
+          <p>Company: {job.company.name}</p>
+        </div>
       ))}
     </div>
   );
