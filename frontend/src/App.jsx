5af24647165ea8ad9c322949984a4a45eab5eff8
Trung Dinh Vu <trungv@metropolia.fi>
2024-09-30 11:34:56 +0300

    Part 2

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,28 +4,31 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/HomePage";
 import AddJobPage from "./pages/AddJobPage";
 import Navbar from "./components/Navbar";
-import NotFoundPage from "./pages/NotFoundPage"
+import NotFoundPage from "./pages/NotFoundPage";
 import JobPage from "./pages/JobPage";
 import EditJobPage from "./pages/EditJobPage";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 
 const App = () => {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Navbar />
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/add-job" element={<AddJobPage />} />
+            <Route path="/jobs/:id" element={<JobPage />} />
+            <Route path="/edit-job/:id" element={<EditJobPage />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="*" element={<NotFoundPage />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
+};
 
-    return (
-      <div className="App">
-        <BrowserRouter>
-          <Navbar />
-          <div className="content">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/add-job" element={<AddJobPage />} />
-              <Route path="/jobs/:id" element={<JobPage />} />
-              <Route path="/edit-job/:id" element={<EditJobPage />} />
-              <Route path="*" element={<NotFoundPage />} />
-            </Routes>
-          </div>
-        </BrowserRouter>
-      </div>
-    );
-  }
-  
-  export default App;
+export default App;
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,13 +1,43 @@
+// const Navbar = () => {
+//   return (
+//     <nav className="navbar">
+//       <h1>Job Search</h1>
+//       <div className="links">
+//         <a href="/">Home</a>
+//         <a href="/add-job">Add Job</a>
+//       </div>
+//     </nav>
+//   );
+// }
+
+// export default Navbar;
+
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+
 const Navbar = () => {
+  const navigate = useNavigate();
+  const handleClick = (e) => {
+    e.preventDefault();
+    localStorage.removeItem("user");
+    navigate("/");
+  };
+
   return (
     <nav className="navbar">
-      <h1>Job Search</h1>
+      <Link to="/">
+        <h1>React Jobs</h1>
+      </Link>
       <div className="links">
-        <a href="/">Home</a>
-        <a href="/add-job">Add Job</a>
+        <div>
+          <Link to="/add-job">Add Job</Link>
+          <Link to="/login">Login</Link>
+          <Link to="/signup">Signup</Link>
+          <button onClick={handleClick}>Log out</button>
+        </div>
       </div>
     </nav>
   );
-}
- 
+};
+
 export default Navbar;
